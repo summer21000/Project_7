c00e0236be6ba4b62dea50192936050e1e3610c8
summer21000 <wkrehreo1999@gmail.com>
2024-12-19 14:45:50 +0900

    2024-12-19 fastapi main.py 생성

---
+++ main.py
@@ -0,0 +1,38 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+import os
+import openai
+from openai import OpenAI
+import sys
+
+import json
+
+import emergency as em
+
+app = FastAPI()
+
+path = './'
+
+openai.api_key = em.load_file(path + 'api_key.txt')
+os.environ['OPENAI_API_KEY'] = openai.api_key
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello":"World"}
+
+
+@app.get("/hospital_by_module")
+def hospital_by_module(request : str, latitude : float, longitude : float):
+    
+    summary = em.text_summary(request)
+    
+    return {"요약" : summary, "latitude" : latitude, "longitude" : longitude}
